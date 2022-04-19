3a4ed56ebbb045cd8201f31dc86a9f5c521614a1
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2022-04-19 20:18:17 +0200

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,11 @@
 module.exports = {
-  purge: {
-    // Specify the paths to all of the template files in your project
-    content: [
-      './src/components/**/*.res',
-      './src/layouts/**/*.res',
-      './src/*.res',
-    ],
-    options: {
-      safelist: ["html", "body"],
-    }
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/components/**/*.res",
+    "./src/layouts/**/*.res",
+    "./src/*.res",
+  ],
   theme: {
-    extend: {
-    },
+    extend: {},
     /* Most of the time we customize the font-sizes,
      so we added the Tailwind default values here for
      convenience */
@@ -23,21 +15,40 @@ module.exports = {
       base: "1rem",
       lg: "1.125rem",
       xl: "1.25rem",
-      '2xl': "1.5rem",
-      '3xl': "1.875rem",
-      '4xl': "2.25rem",
-      '5xl': "3rem",
-      '6xl': "4rem"
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
     },
     /* We override the default font-families with our own default prefs  */
     fontFamily: {
-      'sans':['-apple-system', 'BlinkMacSystemFont', 'Helvetica Neue', 'Arial', 'sans-serif'],
-      'serif': ['Georgia', '-apple-system', 'BlinkMacSystemFont', 'Helvetica Neue', 'Arial', 'sans-serif'], 
-      'mono': [ 'Menlo', 'Monaco', 'Consolas', 'Roboto Mono', 'SFMono-Regular', 'Segoe UI', 'Courier', 'monospace']
+      sans: [
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Helvetica Neue",
+        "Arial",
+        "sans-serif",
+      ],
+      serif: [
+        "Georgia",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Helvetica Neue",
+        "Arial",
+        "sans-serif",
+      ],
+      mono: [
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        "Roboto Mono",
+        "SFMono-Regular",
+        "Segoe UI",
+        "Courier",
+        "monospace",
+      ],
     },
   },
-  variants: {
-    width: ['responsive']
-  },
-  plugins: []
-}
+  plugins: [],
+};
