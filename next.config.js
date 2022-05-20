178261d2c33a6d05b427e5a38b6ae7f326c1fae5
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2022-05-20 23:03:00 +0200

    update deps

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-const bsconfig = require('./bsconfig.json');
-const fs = require("fs");
+const bsconfig = require("./bsconfig.json");
 
 const transpileModules = ["rescript"].concat(bsconfig["bs-dependencies"]);
 const withTM = require("next-transpile-modules")(transpileModules);
 
-const isWebpack5 = true;
 const config = {
   target: "serverless",
   pageExtensions: ["jsx", "js"],
@@ -14,33 +12,31 @@ const config = {
   webpack: (config, options) => {
     const { isServer } = options;
 
-    if (isWebpack5) {
-      if (!isServer) {
-        // We shim fs for things like the blog slugs component
-        // where we need fs access in the server-side part
-        config.resolve.fallback = {
-          fs: false,
-          path: false,
-        };
-      }
-
-      // We need this additional rule to make sure that mjs files are
-      // correctly detected within our src/ folder
-      config.module.rules.push({
-        test: /\.m?js$/,
-        use: options.defaultLoaders.babel,
-        exclude: /node_modules/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
-        }
-      });
+    if (!isServer) {
+      // We shim fs for things like the blog slugs component
+      // where we need fs access in the server-side part
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+      };
     }
-    return config
+
+    // console.log(JSON.stringify(config.module.rules, null, 4));
+
+    // We need this additional rule to make sure that mjs files are
+    // correctly detected within our src/ folder
+    // config.module.rules.push({
+    //   test: /\.m?js$/,
+    //   use: options.defaultLoaders.babel,
+    //   exclude: /node_modules/,
+    //   type: "javascript/auto",
+    //   resolve: {
+    //     fullySpecified: false,
+    //   },
+    // });
+
+    return config;
   },
-  future: {
-    webpack5: isWebpack5
-  }
 };
 
 module.exports = withTM(config);
