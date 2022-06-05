a557c1530cb9220011a3cae81c2e0b36b2a0a10f
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2022-06-05 23:51:17 +0200

    remove lazy load

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,12 @@
 import React, { StrictMode } from "react";
-import dynamic from "next/dynamic";
+import ResApp from "src/App.mjs";
 
 import "../styles/main.css";
-
-const ResAppLazy = dynamic({
-  loader: () => import("src/App.mjs"),
-  loading: () => <div>App Loading...</div>,
-  ssr: false,
-  suspense: true,
-});
 
 export default function App(props) {
   return (
     <StrictMode>
-      <ResAppLazy {...props} />
+      <ResApp {...props} />
     </StrictMode>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,5 @@
-import React from "react";
-import dynamic from "next/dynamic";
-
-const IndexResLazy = dynamic({
-  loader: () => import("src/Index.mjs"),
-  loading: () => <div>Index Loading...</div>,
-  ssr: false,
-  suspense: true,
-});
+import IndexRes from "src/Index.mjs";
 
 export default function Index(props) {
-  return <IndexResLazy {...props} />;
+  return <IndexRes {...props} />;
 }
