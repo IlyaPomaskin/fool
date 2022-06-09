9d12f752d029fac1873d1cc8f3cd09d2a416d7ec
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2022-06-09 16:45:45 +0200

    remove strict mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,8 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import ResApp from "src/App.mjs";
 
 import "../styles/main.css";
 
 export default function App(props) {
-  return (
-    <StrictMode>
-      <ResApp {...props} />
-    </StrictMode>
-  );
+  return <ResApp {...props} />;
 }
